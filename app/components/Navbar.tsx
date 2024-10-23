d8ab4899dac6d196414e63883e4afadb8c1846cb
Mbonu Osi <axleositech@gmail.com>
2024-10-23 13:19:52 +0100

    created and designed the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,10 +15,10 @@ const Navbar = () => {
         <div className={styles.sCon}>
             <img src='/logo.jpg' alt='A logo of our brand' width={80} height={80}/>
             <ul className={`${styles.ul} ${open? styles.open:''}`}>
-                <Link href='/' className={page==='home'?styles.link:''} onClick={()=>{setPage('home'); setOpen(false)}}>Home</Link>
-                <Link href='/product' className={page==='product'?styles.link:''} onClick={()=>{setPage('product'); setOpen(false)}}>Products</Link>
-                <Link href='/about' className={page==='about'?styles.link:''} onClick={()=>{setPage('about'); setOpen(false)}}>About</Link>
-                <Link href='/contact' className={page==='contact'?styles.link:''} onClick={()=>{setPage('contact'); setOpen(false)}}>Contact</Link>
+                <Link href='/' className={page==='home'?styles.link:styles.links} onClick={()=>{setPage('home'); setOpen(false)}}>Home</Link>
+                <Link href='/product' className={page==='product'?styles.link:styles.links} onClick={()=>{setPage('product'); setOpen(false)}}>Products</Link>
+                <Link href='/about' className={page==='about'?styles.link:styles.links} onClick={()=>{setPage('about'); setOpen(false)}}>About</Link>
+                <Link href='/contact' className={page==='contact'?styles.link:styles.links} onClick={()=>{setPage('contact'); setOpen(false)}}>Contact</Link>
             </ul>
         </div>
         
